bea2233d76ed0719b9c2ff46ab5859f98f01dd88
Mauricio Dulce <mauricio@dulce.dev>
2024-04-14 21:25:28 -0500

    Actualización de versiones de PHP y configuración de opcache

--- src/common/etc/entrypoint.d/0-container-info.sh
+++ src/common/etc/entrypoint.d/0-container-info.sh
@@ -2,20 +2,19 @@
 if [ "$LOG_OUTPUT_LEVEL" != "off" ] && [ "$DISABLE_DEFAULT_CONFIG" = false ]; then
 echo '
 --------------------------------------------------------------------
- ____                             ____  _     _        _   _
-/ ___|  ___ _ ____   _____ _ __  / ___|(_) __| | ___  | | | |_ __
-\___ \ / _ \  __\ \ / / _ \  __| \___ \| |/ _` |/ _ \ | | | |  _ \
- ___) |  __/ |   \ V /  __/ |     ___) | | (_| |  __/ | |_| | |_) |
-|____/ \___|_|    \_/ \___|_|    |____/|_|\__,_|\___|  \___/| .__/
-                                                            |_|
 
-Brought to you by serversideup.net
+
 --------------------------------------------------------------------'
 
-echo '
-🙌 To support Server Side Up projects visit:
-https://serversideup.net/sponsor
+PHP_OPCACHE_STATUS=$(php -r 'echo ini_get("opcache.enable");')
 
+if [ "$PHP_OPCACHE_STATUS" = "1" ]; then
+    PHP_OPCACHE_MESSAGE="✅ Enabled"
+else
+    PHP_OPCACHE_MESSAGE="❌ Disabled"
+fi
+
+echo '
 -------------------------------------
 ℹ️ Container Information
 -------------------------------------'
@@ -23,7 +22,13 @@ echo "
 Docker user:   $(whoami)
 Docker uid:    $(id -u "$(whoami)")
 Docker gid:    $(id -g "$(whoami)")
+OPcache:       $PHP_OPCACHE_MESSAGE
 "
+
+if [ "$PHP_OPCACHE_STATUS" = "0" ]; then
+    echo "👉 [NOTICE]: Improve PHP performance by setting PHP_OPCACHE_ENABLE=1 (recommended for production)."
+fi
+
 else
     if [ "$LOG_OUTPUT_LEVEL" = "debug" ]; then
         echo "👉 $script_name: DISABLE_DEFAULT_CONFIG does not equal \"false\", so debug mode will NOT be automatically set."
