455ff344387959ca6d5900f5cfac8569b17a7d7f
Mauricio Dulce <mauricio@dulce.dev>
2024-05-04 22:49:01 -0500

    chore: Update PHP_OPCACHE_STATUS assignment to use single quotes

--- src/common/etc/entrypoint.d/0-container-info.sh
+++ src/common/etc/entrypoint.d/0-container-info.sh
@@ -2,8 +2,7 @@
 if [ "$SHOW_WELCOME_MESSAGE" = "true" ] && [ "$LOG_OUTPUT_LEVEL" != "off" ] && [ "$DISABLE_DEFAULT_CONFIG" = false ]; then
 echo '
 
-
-PHP_OPCACHE_STATUS=$(php -r 'echo ini_get("opcache.enable");')
+PHP_OPCACHE_STATUS=$(php -r "echo ini_get('opcache.enable');")
 
 if [ "$PHP_OPCACHE_STATUS" = "1" ]; then
     PHP_OPCACHE_MESSAGE="✅ Enabled"
